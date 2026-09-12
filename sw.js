3c5fc2380add48746817ef24b0bac26515a19bbb
bamulen-stack <bamulen@gmail.com>
2026-09-12 19:55:17 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='airbnb-repairs-v9-24-custom-dashboard-cover';
+const CACHE='airbnb-repairs-v9-26-correct-mgv-header';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./dashboard-cover.png'];
 
 self.addEventListener('install',event=>{
