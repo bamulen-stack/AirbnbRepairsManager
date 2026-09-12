0c9c257660b0bf61521e36e1f06eb97cf2ee88e9
bamulen-stack <bamulen@gmail.com>
2026-09-13 00:58:17 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='airbnb-repairs-v9-31-mgv-professional-pdf';
+const CACHE='airbnb-repairs-v9-32-paint-technicians';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./dashboard-cover.png'];
 
 self.addEventListener('install',event=>{
