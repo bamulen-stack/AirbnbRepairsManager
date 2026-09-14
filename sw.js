866c8e3c3538421a6286d610eb5206a45e64bfa3
bamulen-stack <bamulen@gmail.com>
2026-09-14 14:24:26 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='airbnb-repairs-v9-34-paint-target-fix';
+const CACHE='airbnb-repairs-v9-35-dashboard-task-buttons';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./dashboard-cover.png'];
 
 self.addEventListener('install',event=>{
