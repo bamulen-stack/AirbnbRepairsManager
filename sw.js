a480308bd518f9069b26b2da6d3190e42328f52e
bamulen-stack <bamulen@gmail.com>
2026-09-22 21:22:36 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,4 @@
-const CACHE='airbnb-repairs-v9-36-long-term';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./dashboard-cover.png'];
-
-self.addEventListener('install',event=>{
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
-});
-
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch',event=>{
-  const req=event.request;
-  if(req.mode==='navigate'){
-    event.respondWith(
-      fetch(req,{cache:'no-store'})
-        .then(resp=>{
-          const copy=resp.clone();
-          caches.open(CACHE).then(c=>c.put('./index.html',copy));
-          return resp;
-        })
-        .catch(()=>caches.match('./index.html'))
-    );
-    return;
-  }
-  event.respondWith(
-    caches.match(req).then(cached=>{
-      const network=fetch(req).then(resp=>{
-        const copy=resp.clone();caches.open(CACHE).then(c=>c.put(req,copy));return resp;
-      }).catch(()=>cached);
-      return cached||network;
-    })
-  );
-});
+const CACHE='airbnb-repairs-v9-37-long-term-pricing';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./dashboard-cover.png','./icon-192.png','./icon-512.png'])))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
